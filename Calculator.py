ead104a69e7b0f9218ca448a85ff64d1729d6449
Moville <80907574+RafflesG@users.noreply.github.com>
2021-03-30 12:35:05 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+print("You will be adding or subtracting 2 numbers together!")
+number_1 = (input("Select a number:"))
+number_2 = (input("Select another number:"))
+answer = int(number_1) + int(number_2)
+print(f"Your answer is {answer}!")
+
+print("you will be multiplying 2 numbers together!")
+number_1 = (input("Select a number:"))
+number_2 = (input("Select another number:"))
+answer = int(number_1) * int(number_2)
+print(f"Your answer is {answer}!")
